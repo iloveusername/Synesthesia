0ae77490e0dfcf84dc9b30eb83c1439b6b8455ad
iloveusername <bchasestair@gmail.com>
2022-12-31 15:29:50 -0800

    Setting up Basic UI

--- app/src/main/java/com/riats/synthesia/MainActivity.java
+++ app/src/main/java/com/riats/synthesia/MainActivity.java
@@ -3,6 +3,9 @@ package com.riats.synthesia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView textView = findViewById(R.id.textView);
+        Button start = findViewById(R.id.start);
+        Button reflect = findViewById(R.id.reflect);
+
+        int state = 0;
+        reflect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (state == 0){
+                    
+                }
+            }
+        });
     }
 }
