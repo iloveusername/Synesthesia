1dbba125dc713756377e7fc2636e33e419d684e4
iloveusername <bchasestair@gmail.com>
2023-01-05 14:58:07 -0800

    Update MainActivity.java

--- app/src/main/java/com/riats/synthesia/MainActivity.java
+++ app/src/main/java/com/riats/synthesia/MainActivity.java
@@ -7,12 +7,9 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.drawable.AnimatedVectorDrawable;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.vosk.Model;
